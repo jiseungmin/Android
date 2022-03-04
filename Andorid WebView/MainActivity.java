898f2694827fa5256c0359cb0fa710a8f89cc99c
jiseungmin <98318326+jiseungmin@users.noreply.github.com>
2022-03-04 16:35:50 +0900

    Android WebView

---
+++ Andorid WebView/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.androidwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.inputmethodservice.Keyboard;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private String url = "https://m.naver.com" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().getJavaScriptEnabled();
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        //webView.setWebViewClient(new WebViewClientclass());
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WebViewClientclass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
